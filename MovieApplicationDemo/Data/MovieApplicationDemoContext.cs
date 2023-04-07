35086911a919e57d643754b494b6c2e7c3c0e889
marcm <marcm@DESKTOP-TKR0368>
2023-04-07 12:11:18 -0700

    Add project files.

---
+++ MovieApplicationDemo/Data/MovieApplicationDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieApplicationDemo.Models;
+
+namespace MovieApplicationDemo.Data
+{
+    public class MovieApplicationDemoContext : DbContext
+    {
+        public MovieApplicationDemoContext (DbContextOptions<MovieApplicationDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieApplicationDemo.Models.Episode> Episode { get; set; } = default!;
+    }
+}
---
+++ MovieApplicationDemo/Models/Episode.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieApplicationDemo.Models
+{
+    public class Episode
+    {
+        public int Id { get; set; }
+        public int Season { get; set; }
+        public int Number { get; set; }
+        public int SeasonEpisodeNumber { get; set; }
+        public DateTime AirDate { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; } 
+
+    }
+}
